2f6921d865fab1b99300cc809639a243ea4665b2
yejin-lim <116084728+yejin-lim@users.noreply.github.com>
2022-11-14 14:46:23 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,29 +1,13 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
+	"log"
 	"net/http"
-	"os"
-	"sort"
-	"strings"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		res := &response{Message: "Hello World"}
-
-		// Beautify the JSON output
-		out, _ := json.MarshalIndent(res, "", "  ")
-
-		// Normally this would be application/json, but we don't want to prompt downloads
-		w.Header().Set("Content-Type", "text/plain")
-
-		io.WriteString(w, string(out))
-
-		fmt.Println("Hello world - the log message")
+		tmpl, err := template.New("index").Parse("<h1>Hello, Go!<h1>")
 	})
 	http.ListenAndServe(":8080", nil)
 }
