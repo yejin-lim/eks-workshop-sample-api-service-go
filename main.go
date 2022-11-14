b19040da989064f6753bdb513c8d62a1ce032267
yejin-lim <116084728+yejin-lim@users.noreply.github.com>
2022-11-14 15:46:19 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,56 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"sort"
+	"strings"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
+
+		f := fib()
+
+		res := &response{Message: "Hello World, I'm new version deployed by the fancy CICD, Hahahaha"}
+
+		for _, e := range os.Environ() {
+			pair := strings.Split(e, "=")
+			res.EnvVars = append(res.EnvVars, pair[0]+"="+pair[1])
+		}
+		sort.Strings(res.EnvVars)
+
+		for i := 1; i <= 90; i++ {
+			res.Fib = append(res.Fib, f())
+		}
+
+		// Beautify the JSON output
+		out, _ := json.MarshalIndent(res, "", "  ")
+
+		// Normally this would be application/json, but we don't want to prompt downloads
+		w.Header().Set("Content-Type", "text/plain")
+
+		io.WriteString(w, string(out))
+
+		fmt.Println("Hello world - the log message")
 	})
+	http.ListenAndServe(":8080", nil)
+}
 
-	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
-		name := r.URL.Path[len("/greet/"):]
-		fmt.Fprintf(w, "Hello %s\n", name)
-	})
+type response struct {
+	Message string   `json:"message"`
+	EnvVars []string `json:"env"`
+	Fib     []int    `json:"fib"`
 
-	http.ListenAndServe(":9990", nil)
+}
 
+func fib() func() int {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
 }
