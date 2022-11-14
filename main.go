892b4c9b7c186a34a5c4abbed8b0a2b314eeaf93
yejin-lim <116084728+yejin-lim@users.noreply.github.com>
2022-11-14 15:08:04 +0900

    Update main.go

--- main.go
+++ main.go
@@ -8,12 +8,14 @@ import (
     "html/template"
 )
  
-func editHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/index/"):]
-    p, err := loadPage(title)
-    if err != nil {
-        p = &Page{Title: title}
-    }
-    t, _ := template.ParseFiles("index.html")
-    t.Execute(w, p)
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	})
+
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello Bar!")
+	})
+
+	http.ListenAndServe(":3000", nil)
 }
