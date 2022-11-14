54d5354eba69d672885bbc932cb10bc77e695e88
yejin-lim <116084728+yejin-lim@users.noreply.github.com>
2022-11-14 14:55:52 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
-
+ 
 import (
-	"log"
-	"net/http"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "html/template"
 )
-
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.New("index")
-	})
-	http.ListenAndServe(":8080", nil)
+ 
+...
+ 
+ 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/index/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    t, _ := template.ParseFiles("index.html")
+    t.Execute(w, p)
 }
-
