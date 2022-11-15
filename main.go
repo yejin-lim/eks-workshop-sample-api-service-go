6471ce1d5150b12aa00ce059760d1f523971113c
yejin-lim <116084728+yejin-lim@users.noreply.github.com>
2022-11-15 18:49:31 +0900

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
+		fmt.Fprintf(w, "Hello World! Hahaha")
 	})
 
 	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
