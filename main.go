6f38c1ccde3810e92dce844e50e0b51e1f25ee6e
yejin-lim <116084728+yejin-lim@users.noreply.github.com>
2022-11-15 18:40:15 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,40 +1,13 @@
 package main
-
+ 
 import (
-	"embed"
-	"html/template"
-	"net/http"
-	"path"
-
-	"github.com/gin-gonic/gin"
+    "net/http"
 )
-
-var (
-	//go:embed web/templates/*.html
-	templatesFS embed.FS
-
-	//go:embed web
-	staticFS embed.FS
-)
-
+ 
 func main() {
-	r := gin.Default()
-
-	LoadHTMLFromEmbedFS(r, templatesFS, "web/templates/*")
-
-	r.GET("/", index)
-	r.GET("/static/*filepath", func(c *gin.Context) {
-		c.FileFromFS(path.Join("/web/", c.Request.URL.Path), http.FS(staticFS))
-	})
-	r.Run()
-}
-
-func index(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", nil)
-}
-
-func LoadHTMLFromEmbedFS(engine *gin.Engine, em embed.FS, pattern string) {
-	templ := template.Must(template.ParseFS(em, pattern))
-
-	engine.SetHTMLTemplate(templ)
+    http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+        w.Write([]byte("Hello World"))
+    })
+ 
+    http.ListenAndServe(":8080", nil)
 }
