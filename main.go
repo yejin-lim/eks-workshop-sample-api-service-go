6cecbc5683d7b7677357cb4a4ebcdedfcaa621d8
yejin-lim <116084728+yejin-lim@users.noreply.github.com>
2022-11-15 15:37:08 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
-  "net/http"
-  "html/template"
-  "github.com/yejin-lim/eks-workshop-sample-api-service-go"
-  "fmt"
+	"embed"
+	"html/template"
+	"net/http"
+	"path"
+
+	"github.com/yejin-lim/eks-workshop-sample-api-service-go"
 )
 
-func main(){
-    http.Handle("/", http.FileServer(http.Dir("/template")))
-    
-    fmt.Println("Serving ...")
-    http.ListenAndServe(":8080", nil)
+var (
+	//go:embed web/template/*.html
+	templateFS embed.FS
+
+	//go:embed web
+	//staticFS embed.FS
+)
+
+func main() {
+	r := gin.Default()
+
+	LoadHTMLFromEmbedFS(r, templateFS, "template/*")
+
+	r.GET("/", index)
+	r.GET("template/*filepath", func(c *gin.Context) {
+		c.FileFromFS(path.Join("/", c.Request.URL.Path), http.FS(templateFS))
+	})
+	r.Run()
+}
+
+func index(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", nil)
+}
+
+func LoadHTMLFromEmbedFS(engine *gin.Engine, em embed.FS, pattern string) {
+	templ := template.Must(template.ParseFS(em, pattern))
+
+	engine.SetHTMLTemplate(templ)
 }
