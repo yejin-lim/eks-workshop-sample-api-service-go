40490d6bf0ec2186ba8652ac8ccd7c1b7bc0a8e3
yejin-lim <116084728+yejin-lim@users.noreply.github.com>
2022-11-15 14:58:04 +0900

    Update main.go

--- main.go
+++ main.go
@@ -7,19 +7,9 @@ import (
   "fmt"
 )
 
-func main() {
-  r := mux.NewRouter()
-  tmpl := template.Must(template.ParseFiles("index.html"))
-
-  r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    tmpl.Execute(w, nil)
-  }).Methods("GET")
-  
-  r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    var details ContactDetails
-    json.NewDecoder(r.Body).Decode(&details) // read body, then decode
-    json.NewEncoder(w).Encode(details) // encode, then send to user
-  }).Methods("POST")
-  
-  http.ListenAndServe(":8080", r)
+func main(){
+    http.Handle("/", http.FileServer(http.Dir("/template")))
+    
+    fmt.Println("Serving ...")
+    http.ListenAndServe(":8080", nil)
 }
