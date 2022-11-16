3921505902475cae54f3d2f56c1e7bf0f6bbbcfd
yejin-lim <116084728+yejin-lim@users.noreply.github.com>
2022-11-16 12:44:48 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,50 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+  "log"
+  "net/http"
+  "net/http/cgi"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
-	})
+  http.Handle("/", &cgi.Handler{
+    Path: "/usr/bin/php-cgi",
+    Dir:  "./",
+    Env: []string{
+      "SCRIPT_FILENAME=F2_TBDEV3054.php5",
+      "REDIRECT_STATUS=trash",
+    },
+  })
 
-	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
-		name := r.URL.Path[len("/greet/"):]
-		fmt.Fprintf(w, "Hello %s\n", name)
-	})
+  // Assumes ../app/app is running in ../app directory
+  //http.Handle("/go-fcgi", &fcgi.Handler{
+  //  Dialer: &fcgi.NetDialer{
+  //    Network: "unix",
+  //    Address: "../app.socket",
+  //  },
+  //})
+  //http.Handle("/go-cgi", &cgi.Handler{
+  //  Path: "../app/app",
+  //  Dir:  "../app/",
+  //  Args: []string{"cgi"},
+  //})
+  //
+  //// Assumes ../app/index.py is running in ../app directory
+  //http.Handle("/py-fcgi", &fcgi.Handler{
+  //  Dialer: &fcgi.NetDialer{
+  //    Network: "unix",
+  //    Address: "../app-py.socket",
+  //  },
+  //})
+  //http.Handle("/py-cgi", &cgi.Handler{
+  //  Path: "../app/index.py",
+  //  Dir:  "../app/",
+  //  Args: []string{"cgi"},
+  //})
 
-	http.ListenAndServe(":8080", nil)
-
+  log.Println("port:8080")
+  err := http.ListenAndServe(":8080", nil)
+  if err != nil {
+    log.Fatal(err.Error())
+  }
 }
