79a3471b1fdc1ceb388fac1a698607508eb84050
yejin-lim <116084728+yejin-lim@users.noreply.github.com>
2022-11-16 11:52:22 +0900

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.LoadHTMLGlob("web/templates/*")
+	r.LoadHTMLGlob("templates/*")
 
 	r.GET("/", func(c *gin.Context) {		
         // OK 이면 index.html파일에 JSON데이터를 넘겨서 보여줌 
