bc91e1cf31d74959776753d56510e57ece65224a
yejin-lim <116084728+yejin-lim@users.noreply.github.com>
2022-11-16 11:55:01 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,24 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-
-	r.LoadHTMLGlob("templates/*")
-
-	r.GET("/", func(c *gin.Context) {		
-        // OK 이면 index.html파일에 JSON데이터를 넘겨서 보여줌 
-		c.HTML(http.StatusOK, "index.html", gin.H{
-				"title": "Home Page",
-			},
-		)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
 	})
 
-	r.Run()
+	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Path[len("/greet/"):]
+		fmt.Fprintf(w, "Hello %s\n", name)
+	})
+
+	http.ListenAndServe(":8080", nil)
 
 }
