6c38f5dbaed4f42b5994bf3528097e1a923b2863
yejin-lim <116084728+yejin-lim@users.noreply.github.com>
2022-11-16 11:47:41 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-     log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("/usr/share/nginx/"))))
+	r := gin.Default()
+
+	r.LoadHTMLGlob("web/templates/*")
+
+	r.GET("/", func(c *gin.Context) {		
+        // OK 이면 index.html파일에 JSON데이터를 넘겨서 보여줌 
+		c.HTML(http.StatusOK, "index.html", gin.H{
+				"title": "Home Page",
+			},
+		)
+	})
+
+	r.Run()
+
 }
